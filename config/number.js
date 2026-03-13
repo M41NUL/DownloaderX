dd1c33af93b324c5a9a2fcd8e1dd8c4095ac1e3b
MAINUL - X <githubmainul@gmail.com>
2026-03-13 20:36:22 +0600

    Create number.js

---
+++ config/number.js
@@ -0,0 +1,10 @@
+/**
+ * =============================================
+ * MAINUL-X BOT CONFIG
+ * =============================================
+ * WhatsApp Number for Pairing Login
+ * Country code required, no +
+ * Example: 8801XXXXXXXXX
+ */
+
+export const WA_NUMBER = "8801858845308"
