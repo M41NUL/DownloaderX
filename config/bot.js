91cf63109fb28f1a2119c216deda50b12215d419
MAINUL - X <githubmainul@gmail.com>
2026-03-14 11:22:20 +0600

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -14,18 +14,7 @@ export const OWNER_ALIAS = "MAINUL - X"
 export const WHATSAPP = "+8801308850528"
 export const TELEGRAM = "@mdmainulislaminfo"
 
-export const GITHUB_USERNAME = "M41NUL"
 export const GITHUB_URL = "https://github.com/M41NUL"
 
 export const EMAIL_PRIMARY = "githubmainul@gmail.com"
-export const EMAIL_SECONDARY = "devmainulislam@gmail.com"
-
-export const LICENSE = "MIT"
-export const COPYRIGHT = "© 2026 MAINUL-X"
-
-export const SUPPORTED_PLATFORMS = [
-"YouTube",
-"Facebook",
-"Instagram",
-"TikTok"
-]
+export const EMAIL_DEV = "devmainulislam@gmail.com"
